072af3cbf5783ef25f91e0735221f805b73fbed3
PerlaAdam <ibrahimragab52@yahoo.com>
2024-04-12 00:50:38 +0200

    hola

--- BaseTest.java
+++ BaseTest.java
@@ -2,15 +2,17 @@ package runCases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class BaseTest {
     WebDriver driver;
 
 @BeforeMethod
     public void setUp(){
-    driver = new ChromeDriver();
+    driver = new EdgeDriver();
     driver.navigate().to("https://parabank.parasoft.com/parabank/login.htm");
 }
 @AfterMethod
