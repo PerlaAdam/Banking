a049e882be88ce3e262ca6630ce6ec24e9c74c8a
PerlaAdam <ibrahimragab52@yahoo.com>
2024-04-23 22:41:48 +0200

    update

--- BillPayTest.java
+++ BillPayTest.java
@@ -1,5 +1,26 @@
 package runCases;
 
-public class BillPayTest extends BaseTest{
+import org.checkerframework.checker.units.qual.C;
+import org.testng.annotations.Test;
+import pages.BillPagePage;
+import pages.LoginPage;
 
+public class BillPayTest extends BaseTest{
+    String PayeeName = "Ibrahim";
+    String Address = "faisal";
+    String City = "Giza";
+    String State = "State";
+    String ZipCode = "54345";
+    String Phone = "01014880895";
+    String Account = "493449";
+    String VerifyAccount = "493449";
+    String Amount = "500";
+
+    @Test
+    public void billPay(){
+        LoginPage loginPage = new LoginPage(driver);
+        BillPagePage billPay = new BillPagePage(driver);
+         billPay.billPay(PayeeName,Address,City,State,ZipCode,Phone,Account,VerifyAccount,Amount);
+         loginPage.navigateToHomePage("ibrahimragab", "Adam_1010");
+    }
 }
--- NewAccountTest.java
+++ NewAccountTest.java
@@ -1,6 +1,13 @@
 package runCases;
 
+import pages.NewAccountPage;
+
 public class NewAccountTest extends BaseTest{
+
+    public void createNewAccount(){
+        NewAccountPage newAccountPage = new NewAccountPage(driver);
+
+    }
 
 
 }
